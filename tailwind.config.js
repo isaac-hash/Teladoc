6bf4bf7c451777eb3da5670e200352943236d56d
Isaac <zikkyychukwudulue@gmail.com>
2024-11-05 03:45:02 +0100

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    content: [],
-    theme: {
-      extend: {},
+  content: [
+    "./src/**/*.{vue,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
     },
-    variants: {
-      extend: {
-        display: ["group-hover"],
-    },
-    },
-    plugins: [],
+  },
+  plugins: [],
 }
-
-
